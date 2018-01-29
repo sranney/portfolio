b8fe821e861646d062a70cdaa1d4e0d93b3989b5
Spencer <spencerranney@gmail.com>
2018-01-29 12:46:02 -0600

    sendgrid

--- routes/routes.js
+++ routes/routes.js
@@ -1,23 +1,4 @@
 module.exports = (app,sgMail)=>{
     //serve the html file for when the user goes to a webpage
-    app.get("/",(req,res)=>{
-        res.sendFile("index.html");
-    });
 
-    //send email
-    app.post("/sendInvite",(req,res) => {
-        const msgSubj = `Connection - ${req.body.name}`;
-        const msgBody = `${req.body.name} at ${req.body.email} wants to connect with you.<br>Here is their message: ${req.body.message}`;
-        
-        const msg = {
-        to: 'spencerranney@gmail.com',
-        cc: req.body.email,
-        from: 'spencer.ranney.developer@gmail.com',
-        subject: msgSubj,
-        html: msgBody,
-        };
-        sgMail.send(msg).catch(err=>console.log(err));
-        
-        res.send(req.body.name);
-    })
 };
--- server.js
+++ server.js
@@ -19,6 +19,27 @@ sgMail.setApiKey(APIKey);
 
 require("./routes/routes.js")(app,sgMail);
 
+app.get("/",(req,res)=>{
+    res.sendFile("index.html");
+});
+
+//send email
+app.post("/sendInvite",(req,res) => {
+    const msgSubj = `Connection - ${req.body.name}`;
+    const msgBody = `${req.body.name} at ${req.body.email} wants to connect with you.<br>Here is their message: ${req.body.message}`;
+    
+    const msg = {
+    to: 'spencerranney@gmail.com',
+    cc: req.body.email,
+    from: 'spencer.ranney.developer@gmail.com',
+    subject: msgSubj,
+    html: msgBody,
+    };
+    sgMail.send(msg).catch(err=>console.log(err));
+    
+    res.send(req.body.name);
+})
+
 //create a port variable
 const port = process.env.PORT||5000;
 //set up server to listen on port
